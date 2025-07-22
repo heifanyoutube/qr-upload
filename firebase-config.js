ea0af2c195e912371fcbc3da903c985adfbe03ad
heifanyoutube <124052031+heifanyoutube@users.noreply.github.com>
2025-07-22 15:21:46 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
-// 將此檔引入 main.js 前面，或直接放到 main.js 裡
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCRS8PIHBinVyWkkJhhjb7-F8MJkqC8ysA",
+  authDomain: "qr-upload-15907.firebaseapp.com",
+  projectId: "qr-upload-15907",
+  storageBucket: "qr-upload-15907.firebasestorage.app",
+  messagingSenderId: "611047398936",
+  appId: "1:611047398936:web:cda1fc318758f1b9bfed6d",
+  measurementId: "G-Q3W49TMFR1"
 };
-
-firebase.initializeApp(firebaseConfig);
